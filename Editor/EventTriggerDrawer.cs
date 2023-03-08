af92a868a564dc26e89a4782c2031758462fcd46
Cielbird <guilhem.ane3@gmail.com>
2023-03-08 16:51:43 -0500

    Improved event listener

---
+++ Editor/EventTriggerDrawer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.UIElements;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace ChickadeeEvents
+{
+    [CustomPropertyDrawer(typeof(EventTrigger))]
+    public class EventTriggerDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            VisualElement container = new VisualElement();
+            // name
+            var eventNames = EventManager.Current.Data.EventNames;
+            var eventDropdown = new DropdownField("Event: ", eventNames, 0);
+            var eventNameProp = property.FindPropertyRelative("EventName");
+            eventDropdown.BindProperty(eventNameProp);
+            container.Add(eventDropdown);
+            // trigger
+            var trigProp = property.FindPropertyRelative("Triggers");
+            PropertyField trigField = new PropertyField(trigProp);
+            container.Add(trigField);
+
+            return container;
+        }
+    }
+}
